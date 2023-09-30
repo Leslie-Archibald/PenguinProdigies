f9b8f296a858f60f437b8a6bbf0a094f7d54ad33
RyanWu471 <98716877+RyanWu471@users.noreply.github.com>
2023-09-30 09:23:59 -0400

    Added missing folders and files

---
+++ public/functions.js
@@ -0,0 +1,3 @@
+function createLink(){
+    document.getElementById("cookie-link").innerHTML = "<ul><li><a href='http://localhost:8080/visit-counter'>Click here to see the /visit-counter page!</a></li></ul>"
+}
