0777d9eef5d802a42caf84ab867c8a03c479ee91
solomonr03 <solomonr@buffalo.edu>
2023-10-13 16:22:16 -0400

    Modified function.js

--- public/functions.js
+++ public/functions.js
@@ -1,3 +1,23 @@
 function createLink(){
     document.getElementById("cookie-link").innerHTML = "<ul><li><a href='http://localhost:8080/visit-counter'>Click here to see the /visit-counter page!</a></li></ul>"
 }
+
+function sendPost() {
+    const titleTextBox = document.getElementById("title-text-box");
+    const title = titleTextBox.value;
+    const descriptionTextBox = document.getElementById("description-text-box");
+    const description = descriptionTextBox.value;
+    titleTextBox.value = "";
+    descriptionTextBox.value = "";
+    const request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            console.log(this.response);
+        }
+    }
+    const username = "Guest"
+    const messageJSON = {"title": title, "username": username, "description": description};
+    request.open("POST", "/chat-message");
+    request.send(JSON.stringify(messageJSON));
+    chatTextBox.focus();
+}
