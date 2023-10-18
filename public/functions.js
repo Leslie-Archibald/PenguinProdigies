a88004bf49dd1b742bc5f8be78a2a19337256080
solomonr03 <solomonr@buffalo.edu>
2023-10-18 19:34:56 -0400

    Added post functions, still in progess

--- public/functions.js
+++ public/functions.js
@@ -2,13 +2,32 @@ function createLink(){
     document.getElementById("cookie-link").innerHTML = "<ul><li><a href='http://localhost:8080/visit-counter'>Click here to see the /visit-counter page!</a></li></ul>"
 }
 
+function chatMessageHTML(messageJSON) {
+    const username = messageJSON['"username"']
+    const title = messageJSON['"title"'];
+    const description = messageJSON['"description"'];
+    let messageHTML = "<br><button onclick='deleteMessage(" + title + ")'>X</button> ";
+    messageHTML += "<span id='message_" + title + "'><b>" + username + "</b>: " + description + "</span>";
+    return messageHTML;
+}
+
+function clearChat() {
+    const chatMessages = document.getElementById("chat-messages");
+    chatMessages.innerHTML = "";
+}
+
+function addMessageToChat(messageJSON) {
+    const chatMessages = document.getElementById("chat-messages");
+    chatMessages.innerHTML += chatMessageHTML(messageJSON);
+    chatMessages.scrollIntoView(false);
+    chatMessages.scrollTop = chatMessages.scrollHeight - chatMessages.clientHeight;
+}
+
 function sendPost() {
     const titleTextBox = document.getElementById("title-text-box");
     const title = titleTextBox.value;
     const descriptionTextBox = document.getElementById("description-text-box");
     const description = descriptionTextBox.value;
-    titleTextBox.value = "";
-    descriptionTextBox.value = "";
     const request = new XMLHttpRequest();
     request.onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200) {
@@ -16,8 +35,24 @@ function sendPost() {
         }
     }
     const username = "Guest"
-    const messageJSON = {"title": title, "username": username, "description": description};
+    const id = this.crypto.randomUUID();
+    const messageJSON = {"title": title, "username": username, "description": description, "id": id};
     request.open("POST", "/chat-message");
     request.send(JSON.stringify(messageJSON));
     chatTextBox.focus();
+}
+
+function updateChat() {
+    const request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            clearChat();
+            const messages = JSON.parse(this.response);
+            for (const message of messages) {
+                addMessageToChat(message);
+            }
+        }
+    }
+    request.open("GET", "/chat-history");
+    request.send();
 }
