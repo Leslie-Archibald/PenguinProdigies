212d9b91b3246b55c17b63326a3da154e2e80d47
RyanWu471 <98716877+RyanWu471@users.noreply.github.com>
2023-10-18 20:18:25 -0400

    Added functions to like.py
    The functions are not currently designed to properly handle the inputs. I don't yet know how the "body" variable will look like.
    
    The code will need to be updated once more information on how user posts will be parsed is obtained

--- util/likes.py
+++ util/likes.py
@@ -1,18 +1,47 @@
+from pymongo import MongoClient
 
+#The "likes" table is going to just consist of {username,postID}.
+#If an element exists in the table, then that means the user with that username has liked the post
+#with that postID
 
-def likes(db, body):
-    #db is the database structure
+def numLikes(db_likes,body):
+    #returns the total number of likes a certain post has recieved
+
+    #These are not real functions, these are just placeholders
+    postID = body.getPostID() 
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    numLikes = db_likes.find({"postID":postID}).count()
+
+    return(numLikes)
+
+def likes(db_likes, body):
+    #db_likes is the database structure, specifically the table to be used for likes
     #body is the string(s) containing the user post info like the username
+    #returns the updated number of likes the post has recieved.
 
     #These are not real functions, these are just placeholders
     postID = body.getPostID() 
     username = body.getUsername()
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-    db
+    if(db_likes.find({"username":username, "postID":postID}).count <= 0 ):
+        #No likes element exists for this user-post pair
+        db_likes.insert({"username":username,
+                        "postID":postID})
+        
+    else: #A user-post pair does exist
 
+        #this is for testing. coment this out later!
+        print(db_likes.find_one({"username":username,
+                                "postID":postID}) )
 
-    
+        #removes the like
+        db_likes.delete({"username":username,
+                        "postID":postID})
+        
+        #will probably need to force some kind of update on the client's POV to show that a post has been "unliked"
+        totalLikes = numLikes(db_likes,body)
 
+    return totalLikes
 
-    return 0    
