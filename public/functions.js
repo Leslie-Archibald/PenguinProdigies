46cd38d11f2768ff1f5d02df7d5ada3ffbc0c502
Leslie-Archibald <lmarchib@buffalo.edu>
2023-10-22 19:41:50 -0400

    Update functions.js

--- public/functions.js
+++ public/functions.js
@@ -5,20 +5,3 @@ function createLink(){
 function usernameTaken(){
     document.getElementById('username-taken')
 }
-
-function chatMessageHTML(messageJSON){
-    // const username = messageJSON.username
-    // const title = messageJSON.title
-    // const description = messageJSON.description
-    // const postID = messageJSON.postID
-
-    // let messageHTML = "<div class='chatPosts'><h1>"+title+"</h1>"+
-}
-
-//TODO function for deleting
-
-
-// TODO function for liking
-
-
-// TODO function for 
