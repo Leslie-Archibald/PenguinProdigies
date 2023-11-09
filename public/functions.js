3de54ac5105828a3d36a60245ec2fb87d4b6c5ea
Leslie-Archibald <lmarchib@buffalo.edu>
2023-11-08 19:45:38 -0500

    finished profile/styling html and css
    CHANGELOG
    index.html:
    - replaced auction house link with profile link
    - removed users online list
    - moved chat form to former users online list location
    - modified chat form to make auctions
    profile.html:
    - completely redid file
    
    profilestyles.css
     - created file
    - finished rough styling for page
    
    styles.css
    - changed styling to accommodate moving form
    
    functions.js
    - no idea probably just had a random space that got added to the end of the line so it's showing up as a file with a "change"

--- public/functions.js
+++ public/functions.js
@@ -2,6 +2,92 @@ function createLink(){
     document.getElementById("cookie-link").innerHTML = "<ul><li><a href='http://localhost:8080/visit-counter'>Click here to see the /visit-counter page!</a></li></ul>"
 }
 
-function usernameTaken(){
-    document.getElementById('username-taken')
+function sendLike(postID){
+    console.log("Got here!!!")
+    const request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            clearChat();
+            const messages = JSON.parse(this.response);
+            for (const message of messages) {
+                addMessageToChat(message);
+            }
+        }
+    }
+    request.open("POST", "/like-message");
+    request.send(postID);
+}
+
+function chatMessageHTML(messageJSON) {
+    const username = messageJSON["username"]
+    const title = messageJSON["title"];
+    const postID = messageJSON["id"];
+    const description = messageJSON["description"];
+    const numLikes = messageJSON["numLikes"];
+    let messageHTML = "<br><button onclick='deleteMessage(" + title + ")'>X</button> ";
+    messageHTML +=  "<span id='" + postID + "'title='" + title + "'><b>" + username + "</b>: " + 
+                        description +
+                        "<button className='likeButton' onClick=\"sendLike(\'"+postID+"\')\"> numLikes = "+numLikes+"</button>" +
+                    "</span>";
+    return messageHTML;
+}
+
+function clearChat() {
+    const chatMessages = document.getElementById("chatHistory");
+    chatMessages.innerHTML = "";
+}
+
+function addMessageToChat(messageJSON) {
+    const chatMessages = document.getElementById("chatHistory");
+    chatMessages.innerHTML += chatMessageHTML(messageJSON);
+    chatMessages.scrollIntoView(false);
+    chatMessages.scrollTop = chatMessages.scrollHeight - chatMessages.clientHeight;
+}
+
+function sendPost() {
+    const titleTextBox = document.getElementById("titleBox");
+    const title = titleTextBox.value;
+    titleTextBox.value = ""
+    const descriptionTextBox = document.getElementById("messageBox");
+    const description = descriptionTextBox.value;
+    descriptionTextBox.value = "";
+    const username = document.getElementById('userid').innerText;
+    const id = this.crypto.randomUUID();
+    const messageJSON = {"title": title, "username": username, "description": description, "id": id};
+    const request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            console.log(this.response);
+        }
+    }
+    request.open("POST", "/chat-message");
+    request.send(JSON.stringify(messageJSON));
+    titleTextBox.focus();
+    descriptionTextBox.focus();
+}
+
+function updateChat() {
+    const request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            clearChat();
+            const messages = JSON.parse(this.response);
+            for (const message of messages) {
+                addMessageToChat(message);
+            }
+        }
+    }
+    request.open("GET", "/chat-history");
+    request.send();
+}
+
+function welcome() {
+    document.addEventListener("keypress", function (event) {
+        if (event.code === "Enter") {
+            sendChat();
+        }
+    });
+
+    updateChat();
+    setInterval(updateChat, 2000);
 }
